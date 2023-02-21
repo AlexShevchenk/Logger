d93c5f72179631c8cb944b0f16a6bbcd7bf5c23b
Oleksandr Shevchenko <105636389+AlexShevchenk@users.noreply.github.com>
2023-02-21 16:05:13 +0200

    Add files via upload

---
+++ Actions.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace LoggerExample
+{
+    public class Actions
+    {
+        private readonly Logger logger;
+
+        public Actions()
+        {
+            logger = Logger.Instance;
+        }
+
+        public Result Start()
+        {
+            logger.Log($"Start method: {nameof(Start)}", LogType.Info);
+            return new Result { Status = true };
+        }
+
+        public Result SkipLogic()
+        {
+            logger.Log($"Skipped logic in method: {nameof(SkipLogic)}", LogType.Warning);
+            return new Result { Status = true };
+        }
+
+        public Result BreakLogic()
+        {
+            Result result = new Result { Status = false, ErrorMessage = "I broke a logic" };
+            logger.Log($"Action failed by a reason: {result.ErrorMessage}", LogType.Error);
+            return result;
+        }
+    }
+
+    public class Result
+    {
+        public bool Status { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
